74ae66a0ce5ee79ad140133c91555a864a5e8e09
Yamelnyk <123339195+Yamelnyk@users.noreply.github.com>
2023-03-16 02:19:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+'use strict'
+
+class People {
+    constructor (gender) {
+        this.gender = gender;
+    }
+    showGender () {
+        if (this.gender === 'woman') {
+            console.log(`It's a woman!`)
+        } else if (this.gender === 'man'){
+            console.log(`It's a man!`)
+        }else if (this.gender === 'girl'){
+            console.log(`It's a girl!`)
+        } else {
+            console.log(`It's a boy!`)
+        }
+    }
+}
+
+
+class Retired extends People {
+    constructor (name, weight, gender) {
+        super (gender);
+        this.name = name; 
+        this.weight = weight;
+        this.person = 'Retired';
+    }
+    showQuestion = () => alert (`${this.name} do you have a dream?`);
+    getName = () => console.log(this.name);
+    phrase = () => alert (`${this.name} weighs ${this.weight} kg.`);
+}
+
+
+class Kids extends Retired {
+    #age;
+    constructor (name, weight, gender, age, nationality) {
+        super (name, weight, gender)
+        this.nationality = nationality;
+        this.#age = 0;
+        this.person = 'Child';
+    }
+
+        showAge () {
+        if (this.#age <= 2) {
+            console.log(`It's a Baby.`)
+        }
+        if (this.#age > 2 && this.#age <= 5 ) {
+            console.log(`It's a Toddler.`)
+        } if (this.#age > 5) {
+            console.log(`It's a Kid`)
+        }
+        }
+    whoYouAre = () => console.log(this.nationality);
+    showQuestion = () => alert (`${this.name} do you have a dream?`);
+}
+
+const yana = new Kids ('Yana', '50', 'girl', '10', 'ukrainian')
+// console.log(yana);
+// yana.showAge(4);
+
+class Workers extends Retired {
+    #height
+    constructor (name, weight, gender, profession) {
+        super (name, weight, gender)
+        this.profession = profession;
+        this.person = 'Worker';
+        this.#height = +prompt('Please enter your height');
+    }
+  showHeight()  {
+            if (this.#height > 180) { 
+                alert('You are tall worker.')
+            } if (this.#height <= 180 && this.#height > 150) {
+                alert('You are of average height.')
+            } if (this.#height <= 150) {
+                this.#showMessage ()
+            }
+        }
+        #showMessage () {
+            alert(`Sorry, You can't work a flight attendant.`)   
+        }   
+        showQuestion = () => alert (`${this.name} do you have a dream?`);     
+}
+
+const emma = new Workers ('Emma', '7', 'woman');
+// console.log(emma)
+// emma.showHeight ()
+// emma.showQuestion ()
+// emma.phrase()
+
